53c1f4282d8ac4dbf00660422e80a9e2372ecebf
billyndong <ndongbilly4@gmail.com>
2026-01-07 12:32:52 +0100

    Test: update footer year to 2026

--- app/views/deffay/footer.php
+++ app/views/deffay/footer.php
@@ -1,6 +1,6 @@
 <!-- footer section start -->
 <footer>
-    <span><a href="#">Deffay</a> | <span class="far fa-copyright"></span> 2022 Tous droits réservés.</span>
+    <span><a href="#">Deffay</a> | <span class="far fa-copyright"></span> 2026 Tous droits réservés.</span>
     <div class="reseaux">
         <a href="#" class="fab fa-facebook-f"></a>
         <a href="#" class="fab fa-instagram"></a>
